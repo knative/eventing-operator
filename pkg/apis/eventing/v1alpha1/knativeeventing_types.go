00056710c99bdcf31026ebdfdef0e83f656c6e91
Vincent <shou@us.ibm.com>
2020-04-20 14:29:43 -0400

    Add support of configuring ConfigMaps with CR (#197)
    * Add support of configuring ConfigMap with CR
    
    * Use 0.13.3 as the previous release version
    
    * Update pkg/reconciler/knativeeventing/common/config_maps_test.go
    
    Co-Authored-By: Matt Moore <mattmoor@vmware.com>
    
    * Update pkg/reconciler/knativeeventing/common/config_maps_test.go
    
    Co-Authored-By: Matt Moore <mattmoor@vmware.com>
    
    * Update pkg/reconciler/knativeeventing/common/config_maps_test.go
    
    Co-Authored-By: Matt Moore <mattmoor@vmware.com>
    
    * Update pkg/reconciler/knativeeventing/common/config_maps_test.go
    
    Co-Authored-By: Matt Moore <mattmoor@vmware.com>
    
    * Update pkg/reconciler/knativeeventing/common/config_maps_test.go
    
    Co-Authored-By: Matt Moore <mattmoor@vmware.com>
    
    * Update pkg/reconciler/knativeeventing/common/config_maps_test.go
    
    Co-Authored-By: Matt Moore <mattmoor@vmware.com>
    
    * Update pkg/reconciler/knativeeventing/common/config_maps_test.go
    
    Co-Authored-By: Matt Moore <mattmoor@vmware.com>
    
    Co-authored-by: Matt Moore <mattmoor@vmware.com>

--- pkg/apis/eventing/v1alpha1/knativeeventing_types.go
+++ pkg/apis/eventing/v1alpha1/knativeeventing_types.go
@@ -60,6 +60,10 @@ type Registry struct {
 // KnativeEventingSpec defines the desired state of KnativeEventing
 // +k8s:openapi-gen=true
 type KnativeEventingSpec struct {
+	// A means to override the corresponding entries in the upstream configmaps
+	// +optional
+	Config map[string]map[string]string `json:"config,omitempty"`
+
 	// A means to override the corresponding deployment images in the upstream.
 	// If no registry is provided, the knative release images will be used.
 	// +optional
