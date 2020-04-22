2e538b3382b3d1e3b7f6748b37789595b7402ad1
Markus Thömmes <markusthoemmes@me.com>
2020-04-22 10:30:40 +0200

    Pass config from main to the controller. (#202)

--- pkg/reconciler/knativeeventing/controller.go
+++ pkg/reconciler/knativeeventing/controller.go
@@ -15,7 +15,6 @@ package knativeeventing
 
 import (
 	"context"
-	"flag"
 	"os"
 	"path/filepath"
 
@@ -25,13 +24,13 @@ import (
 	"k8s.io/apimachinery/pkg/util/sets"
 	"k8s.io/client-go/tools/cache"
 
+	"k8s.io/client-go/rest"
 	"knative.dev/eventing-operator/pkg/apis/eventing/v1alpha1"
 	knativeEventinginformer "knative.dev/eventing-operator/pkg/client/injection/informers/eventing/v1alpha1/knativeeventing"
 	rbase "knative.dev/eventing-operator/pkg/reconciler"
 	deploymentinformer "knative.dev/pkg/client/injection/kube/informers/apps/v1/deployment"
 	"knative.dev/pkg/configmap"
 	"knative.dev/pkg/controller"
-	"knative.dev/pkg/injection/sharedmain"
 )
 
 const (
@@ -39,52 +38,40 @@ const (
 	reconcilerName      = "KnativeEventing"
 )
 
-var (
-	recursive  = flag.Bool("recursive", false, "If filename is a directory, process all manifests recursively")
-	MasterURL  = flag.String("master", "", "The address of the Kubernetes API server. Overrides any value in kubeconfig. Only required if out-of-cluster.")
-	Kubeconfig = flag.String("kubeconfig", "", "Path to a kubeconfig. Only required if out-of-cluster.")
-)
-
-// NewController initializes the controller and is called by the generated code
+// NewControllerWithConfig initializes the controller and is called by the generated code
 // Registers eventhandlers to enqueue events
-func NewController(
-	ctx context.Context,
-	cmw configmap.Watcher,
-) *controller.Impl {
-	knativeEventingInformer := knativeEventinginformer.Get(ctx)
-	deploymentInformer := deploymentinformer.Get(ctx)
+func NewControllerWithConfig(cfg *rest.Config) func(context.Context, configmap.Watcher) *controller.Impl {
+	return func(ctx context.Context, cmw configmap.Watcher) *controller.Impl {
+		knativeEventingInformer := knativeEventinginformer.Get(ctx)
+		deploymentInformer := deploymentinformer.Get(ctx)
 
-	c := &Reconciler{
-		Base:                  rbase.NewBase(ctx, controllerAgentName, cmw),
-		knativeEventingLister: knativeEventingInformer.Lister(),
-		eventings:             sets.String{},
+		c := &Reconciler{
+			Base:                  rbase.NewBase(ctx, controllerAgentName, cmw),
+			knativeEventingLister: knativeEventingInformer.Lister(),
+			eventings:             sets.String{},
+		}
+
+		koDataDir := os.Getenv("KO_DATA_PATH")
+
+		config, err := mfc.NewManifest(filepath.Join(koDataDir, "knative-eventing/"), cfg,
+			mf.UseLogger(zapr.NewLogger(c.Logger.Desugar()).WithName("manifestival")))
+		if err != nil {
+			c.Logger.Error(err, "Error creating the Manifest for knative-eventing")
+			os.Exit(1)
+		}
+
+		c.config = config
+		impl := controller.NewImpl(c, c.Logger, reconcilerName)
+
+		c.Logger.Info("Setting up event handlers for %s", reconcilerName)
+
+		knativeEventingInformer.Informer().AddEventHandler(controller.HandleAll(impl.Enqueue))
+
+		deploymentInformer.Informer().AddEventHandler(cache.FilteringResourceEventHandler{
+			FilterFunc: controller.Filter(v1alpha1.SchemeGroupVersion.WithKind("KnativeEventing")),
+			Handler:    controller.HandleAll(impl.EnqueueControllerOf),
+		})
+
+		return impl
 	}
-
-	koDataDir := os.Getenv("KO_DATA_PATH")
-
-	cfg, err := sharedmain.GetConfig(*MasterURL, *Kubeconfig)
-	if err != nil {
-		c.Logger.Error(err, "Error building kubeconfig")
-	}
-
-	config, err := mfc.NewManifest(filepath.Join(koDataDir, "knative-eventing/"), cfg,
-		mf.UseLogger(zapr.NewLogger(c.Logger.Desugar()).WithName("manifestival")))
-	if err != nil {
-		c.Logger.Error(err, "Error creating the Manifest for knative-eventing")
-		os.Exit(1)
-	}
-
-	c.config = config
-	impl := controller.NewImpl(c, c.Logger, reconcilerName)
-
-	c.Logger.Info("Setting up event handlers for %s", reconcilerName)
-
-	knativeEventingInformer.Informer().AddEventHandler(controller.HandleAll(impl.Enqueue))
-
-	deploymentInformer.Informer().AddEventHandler(cache.FilteringResourceEventHandler{
-		FilterFunc: controller.Filter(v1alpha1.SchemeGroupVersion.WithKind("KnativeEventing")),
-		Handler:    controller.HandleAll(impl.EnqueueControllerOf),
-	})
-
-	return impl
 }
